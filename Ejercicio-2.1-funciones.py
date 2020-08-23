6f984f2d00ad05ebe8315bb7147e43d3a66da044
OscarVasta <oscarvasta@gmail.com>
2020-08-23 19:53:48 -0300

    A este le agregue la lista de materiales

---
+++ Ejercicio-2.1-funciones.py
@@ -0,0 +1,36 @@
+# Ejercicio 2.2 de Oscar Vasta
+# Creando funciones
+""" Crea 3 funciones para calcular descuentos ,
+La 1º funcion calcula un descuento del 10% si en el carrito hay mas de 3 ítems y si el 
+total de compra supero los $10.000 se aplica el 20% de descuento, 
+El 2º array aplica el descuento del costo de envío de $200 si se aplica descuento
+por el 1º caso y la 3º funcion calcula el descuento por puntaje de 10 puntos por cada
+item que suma 10% al descuento que se aplique. """
+# carga oferta de articulos en tablas en memoria
+Desc_Art=(['Pan','Fideos','Leche','Gaseosa','Cerveza','Carne Vacuna','Dentifrico','Desodorante',])
+Precio_Art=([150,47,75,80,135,350,125,185])
+Um_Art=(['  Kgr  ','paquete',' Litro ','botella','botella','  Kgr  ','  Un   ','  Un   '])
+Valor_Art=([])
+# muestra los articulos y precios por pantalla
+print('ITEM | DESCRIPCIÓN | U/M  | PRECIO')
+For index in range(len(Desc_Art)):
+    print '_',index,' :  ',Desc_Art[index],'  |  ',' | ',Um_Art[index],' | ',Precio_Art[index]
+    
+sigue='s'
+cont=0
+while sigue = s or sigue='S':
+    cont=++
+    Cant=input('Ingrese la cantidad de articulos del tipo: ',cont )
+def descuento_cantidad_valor()
+while print('creamos la funcion carta_a()')
+def carta_a(alguien):
+    return "Estimado"+alguien+"me dirijo a Ud. para solicitarle cotizacion de los siguientes materiales:"
+
+print('ahora dirigimos cartas a Juan, Pedro y Jose')
+A = carta_a('Juan')
+print(A)
+A = carta_a('Pedro')
+print(A)
+A = carta_a('Pedro')
+print (A)
+# Fin

b19e6a15c2451dac93d7d9133524c24964fa6344
OscarVasta <oscarvasta@gmail.com>
2020-08-23 19:46:47 -0300

    1º prueba de funciones

---
+++ Ejercicio-2-funciones.py
@@ -0,0 +1,15 @@
+# Ejercicio 2 de Oscar Vasta
+# Creando funciones
+print('Creando Funciones')
+print('creamos la funcion carta_a()')
+def carta_a(alguien):
+    return "Estimado"+alguien+"me dirijo a Ud. para solicitarle cotizacion de los siguientes materiales:"
+
+print('ahora dirigimos cartas a Juan, Pedro y Jose')
+A = carta_a('Juan')
+print(A)
+A = carta_a('Pedro')
+print(A)
+A = carta_a('Pedro')
+print (A)
+# Fin

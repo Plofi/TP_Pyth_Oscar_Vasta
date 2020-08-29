aba1a45892af8d03415cc0ecc90f779bf94d8dd6
OscarVasta <oscarvasta@gmail.com>
2020-08-29 11:15:12 -0300

    Ejercicio que pide 3 Mº los guarda en un Array y los muestra y analiza el Array Le agregue un input al final para que espere a leer los resultados y no se desaparesca al ejecutar la última linea

--- Ejercicio-Oscar_Vasta_1-Curso-Python.py
+++ Ejercicio-Oscar_Vasta_1-Curso-Python.py
@@ -1,9 +1,8 @@
 # EJERCICIO 1.1 PYTHON DE OSCAR VASTA
 # UN PROGRAMA QUE PIDE AL USUARIO 3 NÚMEROS Y LOS GUARDA EN UN ARRAY
-""" eso sería para inicializar el arreglo en 3 elementos nulos si fuera necesario:
-    arreglo = [0 for numero in range(3)] 
-    pero en este caso vamos a inicializar arreglo sin ningun elemento y lo vamos a 
-    ir extendiendo  """
+""" Si fuera necesario inicializar el arreglo en 3 elementos nulos si fuera necesario:  
+        arreglo = [0 for numero in range(3)]
+pero en este caso vamos a inicializar arreglo sin ningun elemento y lo vamos a ir extendiendo  """
 
 print('.'*80)
 print('-'*30+'EJERCICIO DE ARREGLO'+'-'*30)
@@ -22,6 +21,10 @@ while cont <= 2:
     if cont > 2:
         print('el arreglo es : ',arreglo,' cuya cantidad de elementos es :',len(arreglo),' y suma : ',sum(arreglo), '; el valor mas alto : ', max(arreglo), ' y el valor mas bajo : ',min(arreglo))
 print('.'*80)
+
+""" y para que al ejecutrlo desde fuera del Visual estudio Code no se desaparesca el resultado inmediatamente
+ despues de ejecutar la última linea , lo detenemos preguntansdo si vio el resultado  , sal si o no """
+salida = input('si vió el resultado presione cualqier tecla para salir del programa : ',)
 # - END OF FILE -
 
 

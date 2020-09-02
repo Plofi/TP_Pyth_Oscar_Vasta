f71018717d6bea26b394d2fc2e533624b1c28060
OscarVasta <oscarvasta@gmail.com>
2020-09-02 10:07:00 -0300

    Listo y Probado para corregir

---
+++ Oscar_Vasta-Guia_Ejercicios_1.2.py
@@ -0,0 +1,17 @@
+# Oscar Vasta Guia de Ejercicios 1.2 
+# UN PROGRAMA QUE PIDE AL USUARIO 2 NÚMEROS CON UNA FUNCION QUE DEVUELVE EL PRODUCTO 
+def producto(u,v): return u*v
+print('_'*80)
+print(('*'*14),' PROGRAMA QUE PIDE 2 NÚMEROS Y DEVUELVE EL PRODUCTO ',('*'*14))
+print()
+x = int(input("ingrese número x : "))
+y = int(input("ingrese número y : "))
+print('el resultado es : ',producto(x,y))
+print()
+print('_'*80)
+# - END OF FILE -
+
+
+
+
+

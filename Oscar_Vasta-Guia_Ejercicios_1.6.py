ca08b4b790c462a0094d30e748338b74c6bb3182
OscarVasta <oscarvasta@gmail.com>
2020-09-06 11:47:31 -0300

    Listo para corregir

---
+++ Oscar_Vasta-Guia_Ejercicios_1.6.py
@@ -0,0 +1,25 @@
+# ejercicio 1.6 de Oscar Vasta 
+# Dados 2 números calcular, mediante funciones, las cuatro operaciones básicas entre ambos
+def suma(m1, m2): return m1 + m2
+def resta(m1, m2): return m1 - m2
+def producto(m1, m2): return m1 * m2
+def cociente(m1, m2): return m1 / m2
+print('_'*80)
+print('>'*10,' CALCULO DE CUATRO OPERACIONES BÁSICAS ENTRE 2 NÚMEROS','<'*10)
+print()
+n1=float(input('ingrese el 1º número :'))
+n2=float(input('ingrese el 2º número :'))
+print()
+print('La suma es :',suma(n1,n2))
+print('La resta es :',resta(n1,n2))
+print('El producto es :',producto(n1,n2))
+print('El cociente es :',cociente(n1,n2))
+print()
+print('_'*80)
+#
+# END OF FILE
+
+
+
+
+

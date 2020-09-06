b05bb51a5a46f014c38d60a997fd6cf11e5a5706
OscarVasta <oscarvasta@gmail.com>
2020-09-06 12:24:52 -0300

    Le agregue la parte b) del programa que no había leido bien en el enunciado. Ahora si esta cvompleto

--- Oscar_Vasta-Guia_Ejercicios_1.6.py
+++ Oscar_Vasta-Guia_Ejercicios_1.6.py
@@ -15,6 +15,13 @@ print('La resta es :',resta(n1,n2))
 print('El producto es :',producto(n1,n2))
 print('El cociente es :',cociente(n1,n2))
 print()
+n3=int(input('ingrese un número entero del cual desea la tabla de multiplicar:'))
+print('**- TABLA DE MULTIPLICAR DEL ',n3,'-**')
+print(n3,'X 1 =',n3,' '*(20-(len(str((n3))))),'|',n3,'X 2 =',(n3*2))
+print(n3,'X 3 =',(n3*3),' '*(20-(len(str((n3*3))))),'|',n3,'X 4 =',(n3*4))
+print(n3,'X 5 =',(n3*5),' '*(20-(len(str((n3*5))))),'|',n3,'X 6 =',(n3*6))
+print(n3,'X 7 =',(n3*7),' '*(20-(len(str((n3*7))))),'|',n3,'X 8 =',(n3*8))
+print(n3,'X 9 =',(n3*9),' '*(20-(len(str((n3*9))))),'|',n3,'X 10 =',(n3*10))
 print('_'*80)
 #
 # END OF FILE
